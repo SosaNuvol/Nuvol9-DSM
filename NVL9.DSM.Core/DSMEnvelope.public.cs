270134b987a48a70c94628b0a98b0a71652f03e3
Andres Sosa <andres@nuvol9.com>
2025-01-20 09:07:36 -0700

    Add DSMEnvelope, DSMEnvelopeAttribute, and DSMEnvelopeManager classes for enhanced envelope management

---
+++ NVL9.DSM.Core/DSMEnvelope.public.cs
@@ -0,0 +1,184 @@
+﻿namespace NVL9.DSM.Core;
+
+using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
+using NVL9.DSM.Core.Codes;
+using NVL9.DSM.Core.Models;
+using System.Diagnostics;
+using System.Text;
+
+public partial class DSMEnvelope
+{
+    private const int _LINE_LENGTH_OUTPUT = 82;
+
+    public static DSMEnvelope Init(params object[] providedParams)
+    {
+        _initProvidedParams(providedParams, out var showArguments, out var obfuscatedListOfArguments);
+
+        var result = new DSMEnvelope(showArguments, obfuscatedListOfArguments);
+        var stackTrace = new StackTrace();
+        var frames = stackTrace.GetFrames();
+
+        result.CodeBlockInfo = new CodeBlock(frames[_ONE_DEEP_INTO_STACK], providedParams);
+
+        return result;
+    }
+
+
+    public bool IsSuccessful()
+    {
+        return Code == DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.GEN_COMMON_00000);
+    }
+
+    public bool IsInInitializedState()
+    {
+        return Code == DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.GEN_COMMON_00001);
+    }
+
+    public DSMEnvelope Success(bool outputEnvelop = false)
+    {
+        _calculateExecutionTime();
+
+        if (!FreezeStatus && IsInInitializedState())
+        {
+            Code = DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.GEN_COMMON_00000);
+            DTOMessage = Code.ErrorMessage;
+        }
+
+        if (outputEnvelop) PrintEnvelop();
+
+        return this;
+    }
+
+    public void FinishLifeCycle()
+    {
+        PrintEnvelop();
+    }
+
+    public DSMEnvelope ReBase(IDSMEnvelope envelop, bool calculateExecutionTime = true)
+    {
+        if (calculateExecutionTime) _calculateExecutionTime();
+
+        Code = envelop.Code;
+        DTOMessage = envelop.DTOMessage;
+        envelop.SetParentID(RootEnvelopID);
+        ErrorIEID = envelop.ErrorIEID;
+
+        envelop.PrintEnvelop();
+
+        return this;
+    }
+
+    public void SetState(DSMEnvelopeCode code, string message)
+    {
+        Code = code;
+        DTOMessage = message;
+
+        if (Code != DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.GEN_COMMON_00000))
+        {
+            ErrorIEID = UniqueIEID;
+        }
+    }
+
+    public void CaptureException(Exception ex)
+    {
+        _calculateExecutionTime();
+        ErrorIEID = UniqueIEID;
+        Code = DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.API_APPVLD_02000);
+        DTOMessage = ex.Message;
+    }
+
+    public void CaptureException(SqlException ex)
+    {
+        _calculateExecutionTime();
+        ErrorIEID = UniqueIEID;
+        Code = DSMEnvelopeCodeManager.Manager.Find(DSMEnvelopeCodeEnum.API_DATABASE_03020);
+        DTOMessage = ex.Message;
+    }
+
+    public bool ExecuteTry(IDSMEnvelope envelop)
+    {
+        _calculateExecutionTime();
+        return ReBase(envelop, false).IsSuccessful();
+    }
+
+    public void SetParentID(string parentId)
+    {
+        ParentEnvelopID = parentId;
+    }
+
+    public void SetFreezeStatus(bool status)
+    {
+        FreezeStatus = status;
+    }
+
+    public void SetFreezeStatus(DSMEnvelope senderEnvelope)
+    {
+        FreezeStatus = senderEnvelope.FreezeStatus;
+        ErrorIEID = senderEnvelope.ErrorIEID;
+        Code = senderEnvelope.Code;
+        DTOMessage = senderEnvelope.DTOMessage;
+    }
+
+    internal void SetApiTraceId(string? apiTraceId)
+    {
+        ApiTraceId = apiTraceId;
+    }
+
+    internal void SetIdempotencyKeyId(string? idempotencyKeyId)
+    {
+        IdempotencyKeyId = idempotencyKeyId;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"|| {new string('*', _LINE_LENGTH_OUTPUT)} ||");
+
+        sb.AppendLine($"||                UniqueIEID: {UniqueIEID}");
+        sb.AppendLine($"||           SourceErrorIEID: {ErrorIEID}");
+        _addHeaderValues(sb);
+        sb.AppendLine($"||   (Private) RootEnvelopID: {RootEnvelopID}");
+        sb.AppendLine($"|| (Private) ParentEnvelopID: {ParentEnvelopID}");
+        sb.AppendLine($"||            Execution Time: {ExecutionTime ?? "0"}ms");
+        sb.AppendLine($"||                      Code: {Code}");
+        sb.AppendLine($"||              Code Message: {Code.ErrorMessage}");
+        sb.AppendLine($"||                   Message: {DTOMessage}");
+        sb.AppendLine($"||                Class Name: {CodeBlockInfo.ClassName}");
+        sb.AppendLine($"||                    Method: {CodeBlockInfo.Method}");
+
+        sb.Append($"|| {new string('*', _LINE_LENGTH_OUTPUT)} ||");
+
+        return sb.ToString();
+    }
+
+    private void _addHeaderValues(StringBuilder sb)
+    {
+        if (ApiTraceId == null && IdempotencyKeyId == null) return;
+
+        if (ApiTraceId != null)
+        {
+            sb.AppendLine($"||              API-Trace-Id: {ApiTraceId}");
+        }
+        if (IdempotencyKeyId != null)
+        {
+            sb.AppendLine($"||        Idempotency-Key-Id: {IdempotencyKeyId}");
+        }
+    }
+
+    public string ToRawJson()
+    {
+        return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
+
+
+    public void PrintEnvelop()
+    {
+        _setForgroundColor();
+
+        Console.WriteLine(ToString());
+
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ NVL9.DSM.Core/DSMEnvelopeManager.cs
@@ -0,0 +1,77 @@
+﻿namespace NVL9.DSM.Core;
+
+public partial class DSMEnvelopeManager
+{
+    private static DSMEnvelopeManager _instance = null!;
+
+    private readonly Stack<DSMEnvelope> _envelopes = null!;
+
+    public static DSMEnvelopeManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new DSMEnvelopeManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    private DSMEnvelopeManager()
+    {
+        _envelopes = new Stack<DSMEnvelope>();
+    }
+
+    public void PushEnvelope(DSMEnvelope envelop)
+    {
+        _tieToParent(envelop);
+        _envelopes.Push(envelop);
+    }
+
+    public DSMEnvelope? PopEnvelope()
+    {
+        return _envelopes.Count == 0 ? null : _envelopes.Pop();
+    }
+
+    public DSMEnvelope? PeekEnvelope()
+    {
+        return _envelopes.Count == 0 ? null : _envelopes.Peek();
+    }
+
+    private void _tieToParent(DSMEnvelope envelope)
+    {
+        var parent = PeekEnvelope();
+        if (parent == null) return;
+
+        envelope.SetParentID(parent.RootEnvelopID);
+    }
+
+    public void FreezeStackWith(DSMEnvelope senderEnvelope)
+    {
+        var temp = new Stack<DSMEnvelope>();
+        while (_envelopes.Count > 0)
+        {
+            var envelope = _envelopes.Pop();
+
+            // Modify the envelope here
+            if (envelope.UniqueIEID == senderEnvelope.UniqueIEID)
+            {
+                envelope.SetFreezeStatus(true);
+            }
+            else
+            {
+                envelope.SetFreezeStatus(senderEnvelope);
+            }
+
+            temp.Push(envelope);
+        }
+
+        // Rehydratge the stack
+        while (temp.Count > 0)
+        {
+            _envelopes.Push(temp.Pop());
+        }
+    }
+}
